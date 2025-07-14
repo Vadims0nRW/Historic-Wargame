c94d1891ac432a49aa0e688026933122ff78ceae
r0mkins0n <romanursin640@gmail.com>
2025-07-14 19:28:37 +0300

    обновление ресурсов по ID

--- Assets/Scripts/Interaction/Resource.cs
+++ Assets/Scripts/Interaction/Resource.cs
@@ -2,20 +2,60 @@ using UnityEngine;
 
 public class Resource : MonoBehaviour
 {
+    public int max_res;
+    public int current_res;
+    public int current_wood;
+    public int current_rock;
+    public int current_iron;
     // Start is called before the first frame update
     void Start()
     {
-        
+        current_res = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void PickUp(GameObject user)
     {
 
     }
+    public void Add_Resource(int id, int res)
+    {
+        switch (id)
+        {
+            case 1:
+                current_wood = current_wood + res;
+                break;
+            case 2:
+                current_rock = current_rock + res;
+                break;
+            case 3:
+                current_iron = current_iron + res;
+                break;
+        }
+
+
+
+    }
+    public void Remove_res(int id, int res)
+    {
+        switch (id)
+        {
+            case 1:
+                current_wood = current_wood - res;
+                break;
+            case 2:
+                current_rock = current_rock - res;
+                break;
+            case 3:
+                current_iron = current_iron - res;
+                break;
+        }
+    }
+    
+
 }
